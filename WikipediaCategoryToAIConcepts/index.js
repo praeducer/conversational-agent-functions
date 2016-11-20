d3f741388181b4eed379923784eab9674a840571
Paul Prae <paul.prae@outlook.com>
2016-11-19 22:48:04 -0500

    added in methods for querying categories and extracting page ids and page titles.

--- WikipediaCategoryToAIConcepts/index.js
+++ WikipediaCategoryToAIConcepts/index.js
@@ -1,24 +1,66 @@
 var request = require('request');
 
+var getPageidsByCategoryTitleUrl = 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=categorymembers&cmtitle=';
+var getSubCategoryByCategoryTitleUrl = 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=categorymembers&cmtype=subcat&cmtitle='
+var getPagesByPageidsUrl = 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&pageids=';
+
+// Takes in the string of the category title e.g. "Category:Computer vision"
+var getPageidsByCategoryTitle = function(category){
+    context.log('[getPageidsByCategoryTitle] start ' + category);
+
+    category = encodeURIComponent(category);
+    var pageids = [];
+
+    // TODO: await async?
+    return requestPageidsByCategoryTitle(category, 'continue=', pageids);
+}
+
+var requestPageidsByCategoryTitle = function(category, continueParam, pageids){
+    var url = getPageidsByCategoryTitleUrl.concat(category + "&" + continueParam);
+    request(url, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            context.log('[getPageidsByCategoryTitle] request ' + url);
+
+            body = JSON.parse(body);
+            body.query.categorymembers.forEach(function(element) {
+                pageids.push(element.pageid);
+            });
+
+            // TODO: await async?
+            return requestPageidsByCategoryTitle(category, encodeURIComponent(JSON.stringify(body.continue)), pageids);
+        }
+    });
+}
+
+// TODO: Handle continues
+var getPagesByPageids = function(pageids){
+    context.log('[getPagesByPageids] start');
+
+    var pageidsStr = pageids.join("|");
+    var url = getPagesByPageidsUrl.concat(pageidsStr);
+
+    request(url, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            context.log('[getPagesByPageids] request ' + url);
+
+            body = JSON.parse(body);
+            body.query.pages.forEach(function(element) {
+                context.log(element.title);
+            });
+        }
+    });
+}
+
 module.exports = function (context, req) {
 
     context.log('WikipediaCategoryToAIConcepts JavaScript HTTP trigger function processed a request.');
 
     if (req.query.category || (req.body && req.body.category)) {
-
-        var url = 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=';
-        var category = encodeURIComponent(req.query.category || req.body.category);
-        url = url.concat(category);
-
-        request(url, function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                context.log('Request successful for category: ' + category);
-                res = {
-                    // status: 200, /* Defaults to 200 */
-                    body: body
-                };
-            }
-        })
+        var pageids = getPageidsByCategoryTitle('Category:' + category);
+        getPagesByPageids(pageids);
+        res = {
+            body: "WikipediaCategoryToAIConcepts success"
+        }
     }
     else {
         res = {
