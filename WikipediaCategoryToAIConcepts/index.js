73296923db4a07b940d0f43d3e1cc851d7aa8664
Paul Prae <paul.prae@outlook.com>
2016-11-19 20:33:42 -0500

    added in packag.json, references, and test request to wikipedia

--- WikipediaCategoryToAIConcepts/index.js
+++ WikipediaCategoryToAIConcepts/index.js
@@ -1,16 +1,29 @@
-module.exports = function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+var request = require('request');
 
-    if (req.query.name || (req.body && req.body.name)) {
-        res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
+module.exports = function (context, req) {
+
+    context.log('WikipediaCategoryToAIConcepts JavaScript HTTP trigger function processed a request.');
+
+    if (req.query.category || (req.body && req.body.category)) {
+
+        var url = 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=';
+        var category = encodeURIComponent(req.query.category || req.body.category);
+        url = url.concat(category);
+
+        request(url, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                context.log('Request successful for category: ' + category);
+                res = {
+                    // status: 200, /* Defaults to 200 */
+                    body: body
+                };
+            }
+        })
     }
     else {
         res = {
             status: 400,
-            body: "Please pass a name on the query string or in the request body"
+            body: "Please pass a category on the query string or in the request body"
         };
     }
     context.done(null, res);
