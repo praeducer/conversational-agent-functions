c0bdb7d661c0341f33e168a9c994c8df72c8ca68
Paul Prae <paul.prae@outlook.com>
2016-11-21 12:01:13 -0500

    simplified query method that checks for dups. made quotes consistent. fixed some prints.

--- InsertAIConcept/index.js
+++ InsertAIConcept/index.js
@@ -1,15 +1,15 @@
-"use strict";
+'use strict';
 
-const documentClient = require("documentdb").DocumentClient;
+const documentClient = require('documentdb').DocumentClient;
 const url = require('url');
 const path = require('path');
 var config = require(path.resolve(__dirname, 'config'));
 
 var context;
-var client = new documentClient(config.endpoint, { "masterKey": config.primaryKey });
+var client = new documentClient(config.endpoint, { 'masterKey': config.primaryKey });
 var HttpStatusCodes = { NOTFOUND: 404 };
-var databaseUrl = `dbs/${config.database.id}`;
-var collectionUrl = `${databaseUrl}/colls/${config.collection.id}`;
+var databaseUrl = 'dbs/${config.database.id}';
+var collectionUrl = '${databaseUrl}/colls/${config.collection.id}';
 
 // TODO: Handle batches of documents
 // TODO: Only updated if a revision was made since the last time the script was ran: https://www.mediawiki.org/wiki/API:Revisions
@@ -22,34 +22,35 @@ module.exports = function (cntxt, req) {
             .then(function(results){
                 // If there is not an entry already         
                 if(results && results.length == 0){
-                    context.log("[InsertAIConcept] inserting " + req.body);
+                    context.log('[InsertAIConcept] inserting ' + req.body.source.pageid);
                     context.res = {
                         // status: 200, /* Defaults to 200 */
-                        body: "[InsertAIConcept] inserting " + req.body
+                        body: '[InsertAIConcept] inserting ' + req.body.source.pageid
                     };
                     context.bindings.AIConceptDocument = req.body;
                 }
                 else
                 {
-                    context.log("[InsertAIConcept] duplicate " + JSON.stringify(req.body));
+                    context.log('[InsertAIConcept] duplicate ' + req.body.source.pageid);
                     context.res = {
                         // TODO: A duplicate is ok but we should let the client know through a more useful status code
                         status: 200,
-                        body: "[InsertAIConcept] duplicate " + req.body
+                        body: '[InsertAIConcept] duplicate ' + req.body.source.pageid
                     };
                 }
                 context.done(null, context.res);
             })
             .catch(function(err){
-                context.log('[InsertAIConcept] rejected ' + JSON.stringify(req.body));
+                context.log('[InsertAIConcept] rejected ' + req.body.source.pageid);
                 context.log(err);
+                // TODO: Is this redundant? Does catch happen even if then runs?
                 context.done(null, context.res);
             });
     }
     else {
         context.res = {
             status: 400,
-            body: "Missing source.pageid or title or extract in the request body"
+            body: 'Missing source.pageid or title or extract in the request body'
         };
         context.done(null, context.res);
     }
@@ -64,14 +65,8 @@ function QueryCollection(pageid) {
             collectionUrl,
             'SELECT * FROM AIConcept AIC WHERE AIC.source.pageid = '+ pageid
         ).toArray((err, results) => {
-            if (err) reject(err)
-            else {
-                for (var queryResult of results) {
-                    let resultString = JSON.stringify(queryResult);
-                    context.log('[QueryCollection] returned ${resultString}');
-                }
-                resolve(results);
-            }
+            if (err) reject(err);
+            else resolve(results);
         });
     });
 };
