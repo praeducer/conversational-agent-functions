cbda4496502e7d60ed203d406f58908d4df9cb6c
Austin New <literalsands@gmail.com>
2017-10-18 15:58:57 -0700

    removed extra try catch loop. use query parameters to avoid sql injection.

--- InsertAIConcept/index.js
+++ InsertAIConcept/index.js
@@ -1,63 +1,75 @@
-'use strict';
+"use strict";
 
-const documentClient = require('documentdb').DocumentClient;
-const url = require('url');
-const path = require('path');
-var config = require(path.resolve(__dirname, 'config'));
+const documentClient = require("documentdb").DocumentClient;
+const url = require("url");
+const path = require("path");
+var config = require(path.resolve(__dirname, "config"));
 
 var context;
-var client = new documentClient(config.endpoint, { 'masterKey': config.primaryKey });
+var client = new documentClient(config.endpoint, {
+    masterKey: config.primaryKey
+});
 var HttpStatusCodes = { NOTFOUND: 404 };
-var databaseUrl = 'dbs/' + config.database.id;
-var collectionUrl = databaseUrl + '/colls/' + config.collection.id;
+var databaseUrl = "dbs/" + config.database.id;
+var collectionUrl = databaseUrl + "/colls/" + config.collection.id;
 
 // TODO: Handle batches of documents
 // TODO: Only updated if a revision was made since the last time the script was ran: https://www.mediawiki.org/wiki/API:Revisions
-module.exports = function (context, req) {
-    context.log('[InsertAIConcept] JavaScript HTTP trigger function processed a request.');
+module.exports = function(context, req) {
+    context.log(
+        "[InsertAIConcept] JavaScript HTTP trigger function processed a request."
+    );
 
-    if (req.body && req.body.source && req.body.source.pageid && req.body.title && req.body.extract) {
+    if (
+        req.body &&
+        req.body.source &&
+        req.body.source.pageid &&
+        req.body.title &&
+        req.body.extract
+    ) {
         QueryCollection(req.body.source.pageid)
-            .then(function(results){
+            .then(function(results) {
                 // If there is not an entry already
-                if(results && results.length == 0){
-                    context.log('[InsertAIConcept] inserting ' + req.body.source.pageid);
+                if (results && results.length == 0) {
+                    context.log.info(
+                        "[InsertAIConcept] inserting " + req.body.source.pageid
+                    );
                     context.bindings.AIConceptDocument = req.body;
+                } else {
+                    context.log.info(
+                        "[InsertAIConcept] duplicate " + req.body.source.pageid
+                    );
                 }
-                else
-                {
-                    context.log('[InsertAIConcept] duplicate ' + req.body.source.pageid);
-                }
-                context.done();
             })
-            .catch(function(err){
-                context.log('[InsertAIConcept] rejected ' + req.body.source.pageid);
-                context.log(err);
+            .catch(context.done)
+            .catch(function(err) {
+                context.log(
+                    "[InsertAIConcept] rejected " + req.body.source.pageid
+                );
+                context.log.error(err);
             });
     } else {
-        context.log('[InsertAIConcept] missing body');
+        context.log("[InsertAIConcept] missing body");
         context.done();
     }
 };
 
 // https://docs.microsoft.com/en-us/azure/documentdb/documentdb-nodejs-get-started
 function QueryCollection(pageid) {
-    context.log('[QueryCollection] pageid ' + pageid + ' index ' + config.collection.id);
+    context.log(
+        "[QueryCollection] pageid " + pageid + " index " + config.collection.id
+    );
 
-    return new Promise((resolve, reject) => {
-        try {
-            client.queryDocuments(
-                collectionUrl,
-                'SELECT * FROM AIConcept AIC WHERE AIC.active = true AND AIC.source.pageid ='+ pageid
-            ).toArray((err, results) => {
+    return new Promise(function(resolve, reject) {
+        client
+            .queryDocuments(collectionUrl, {
+                query:
+                    "SELECT * FROM AIConcept AIC WHERE AIC.active = true AND AIC.source.pageid = @pageid",
+                parameters: [{ name: "pageid", value: pageid }]
+            })
+            .toArray(function(err, results) {
                 if (err) reject(err);
                 else resolve(results);
             });
-        } catch (e) {
-            context.log('[QueryCollection] exception');
-            context.log(e);
-            if(err) context.log(err);
-            reject(e);
-        }
     });
-};
+}
